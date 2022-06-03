fe6b82fc4cd530c621fe35a370991c1e808fab65
Patrick Ramser <pramser@gmail.com>
2022-06-03 14:15:35 -0400

    Add activity indicator to loading state

--- components/LoadingState.jsx
+++ components/LoadingState.jsx
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
 export default function LoadingState() {
   return (
     <View style={styles.container}>
+      <ActivityIndicator size="large" />
       <Text>Loading...</Text>
     </View>
   );
